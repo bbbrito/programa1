8e1e5a850b2405586ea5ee0484449ffba6c915a6
Brynner Brito <brynner_engc@hotmail.com>
2017-10-27 00:22:02 -0300

    Pede para o usuário digitar dois valores e informa qual o valor é maior

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 
+#include <iostream>
+using std::cout;
+using std::cin;
+
 int main(int argc, char **argv)
 {
 	int valor, contador, peso;
@@ -19,6 +23,24 @@ int main(int argc, char **argv)
         valor = valor + contador;
     }
     
+    cout << "\n\n";
+
+    //Pede para o usuário digitar dois valores e informa qual o valor é maior.
+    int valor1;
+    int valor2;
+    
+    cout << "Entre com o primeiro valor: ";
+    cin >> valor1;
+    
+    cout << "Entre com o segundo valor: ";
+    cin >> valor2;
+    
+    if ( valor1 > valor2)
+        cout << valor1 << " É o maior valor";
+        
+    else
+        cout << valor2 << " É o maior valor";
+
     
     system ("pause");    
 	return 0;
